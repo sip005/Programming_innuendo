ae358ed5bd4614290ff372f8f0757fb76d4b2824
P[-_-]K <you@example.com>
2023-11-20 00:36:58 +0600

    binary tee
    python3
    
    Ref#0001

---
+++ language_specific/python3/DSA_implementations_python3/binary_tree.py
@@ -0,0 +1,10 @@
+class Node(object):
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class BinaryTree(object):
+    def __init__(self, root):
+        self.root = Node(root)
